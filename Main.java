38d9bef272325120234b4c5db3e07eae745977e6
kev-zav <zavalak2811@gmail.com>
2026-07-14 00:36:51 -0500

    Added cases 3 4 and 5. Search, remove and exit

--- Main.java
+++ Main.java
@@ -37,9 +37,47 @@ public class Main {
                     Contact newContact = new Contact(name, phoneNumber, email);
                     contacts.add(newContact);
                     break;
+
+                case 3: 
+                    System.out.println("Enter a contact to search: ");
+                    String contactSearched = scanner.nextLine();
+                    boolean found = false;
+                    for (Contact c : contacts) {
+                        if (contactSearched.equals(c.getName())) {
+                            found = true;
+                            c.viewContact();
+                        }
+                    }
+                    if (!found) {
+                        System.out.println("Contact not found.");
+                    }
+                    break;
+
+                case 4: 
+                    System.out.println("Enter a contact to remove");
+                    String contactRemove = scanner.nextLine();
+                    boolean found2 = false;
+                    Contact toRemove = null;
+                    for (Contact c : contacts) {
+                        if (contactRemove.equals(c.getName())) {
+                            found2 = true;
+                            toRemove = c;
+                        }
+                    }
+                    if (!found2) {
+                        System.out.println("Contact not found.");
+                    }
+                    contacts.remove(toRemove);
+                    break;
+
+                case 5: 
+                    running = false;
+                    break;
+                    
             }
         }
-
         scanner.close();
+        }
     }
-}
+
+    
